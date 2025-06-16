8e77adac826f5f7ee363e06a1fd777daacb33991
Ada Vitória <adavitoriapereiraferreira@gmail.com>
2025-06-15 22:18:28 -0300

    ajustes no drpmenu, fundo branco e retiraar configs

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -65,7 +65,11 @@ export function Header({ user }: HeaderProps) {
                   </Avatar>
                 </Button>
               </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56" align="end" forceMount>
+              <DropdownMenuContent
+                className="w-56  bg-white"
+                align="end"
+                forceMount
+              >
                 <DropdownMenuLabel className="font-normal">
                   <div className="flex flex-col space-y-1">
                     <p className="text-sm font-medium leading-none">
@@ -80,10 +84,6 @@ export function Header({ user }: HeaderProps) {
                 <DropdownMenuItem>
                   <UserIcon className="mr-2 h-4 w-4" />
                   <span>Perfil</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Settings className="mr-2 h-4 w-4" />
-                  <span>Configurações</span>
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem onClick={logout} className="text-red-600">
